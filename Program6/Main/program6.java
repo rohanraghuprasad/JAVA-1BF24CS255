ac664e639b99cf4cd33d59c87049059ded072083
rohanraghuprasad <rohanraghu.cs24@bmsce.ac.in>
2025-11-25 10:52:05 +0530

    Add files via upload

---
+++ Program6/Main/program6.java
@@ -0,0 +1,12 @@
+import SEE.External;
+
+class program6{
+    public static void main(String args[]){
+        External e=new External();
+        e.inputDetails();
+        e.CIEmarks();
+        e.SEEmarks();
+        e.Details();
+        e.Final_marks(e);
+    }
+}
