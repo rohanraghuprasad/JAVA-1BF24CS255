c846b205f1de2a07ea634f8f5e79e9dbed494d84
rohanraghuprasad <rohanraghu.cs24@bmsce.ac.in>
2025-12-04 18:43:36 +0530

    Update Internal.java

--- Program6/CIE/Internal.java
+++ Program6/CIE/Internal.java
@@ -1,5 +1,19 @@
 package CIE;
+
 import java.util.Scanner;
-public class Internal
-{    
+
+public class Internals extends Student{
+    public int marksin[] = new int[5];
+
+    public void CIEmarks(){
+        Scanner scnr=new Scanner(System.in);
+        int i;
+        System.out.println("Enter CIE marks:");
+        for(i=0;i<5;i++){
+            System.out.print("Enter marks for subject "+(i+1)+":");
+            marksin[i]=scnr.nextInt();
+            scnr.nextLine();
+        }
+    }
+
 }
